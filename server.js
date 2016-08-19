1ad488716bf31a8098b65a995f219621e2f41542
Alvaro Martinez <alvaromartinez@Alvaros-MBP-2.home>
2016-08-19 12:57:45 -0400

    route changes

--- server.js
+++ server.js
@@ -47,6 +47,10 @@ app.get('/questions', function(req, res){
 });
 });
 
+app.get('/', function(req, res){
+		res.sendFile(path.join(__dirname + '/public/index.html'));
+	});
+
 
 app.listen(PORT, function(){
 	console.log('App listening on PORT ' + PORT);
